5503ebcc27854520b3368b4b34c6716f50b11ee0
Steven Hepting <shepting@gmail.com>
2021-05-02 23:20:18 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function () {
+    joystickbit.Vibration_Motor(100)
+    basic.showString("E")
+    radio.sendString("E")
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.down, function () {
+    joystickbit.Vibration_Motor(100)
+    basic.showString("F")
+    radio.sendString("F")
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
+    joystickbit.Vibration_Motor(100)
+    basic.showString("D")
+    radio.sendString("D")
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function () {
+    joystickbit.Vibration_Motor(100)
+    basic.showString("C")
+    radio.sendString("C")
+})
+joystickbit.initJoystickBit()
+music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+basic.forever(function () {
+	
+})
